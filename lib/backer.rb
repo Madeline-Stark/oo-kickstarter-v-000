abf2e2d92e2f59b7601795a038c4f22d146bed2a
Madeline Stark <madeline.stark1@gmail.com>
2018-04-24 22:32:54 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,3 @@
-
-#When a backer has added a project to its list of backed projects, that project should also add the backer to its list of
-#backers. Refer back to the Code Along about Collaborating Objects.
-
 class Backer
   attr_accessor :name, :backed_projects
 
